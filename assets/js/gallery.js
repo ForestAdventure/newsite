57c91537c72aad48b86cf8ed84b1b44d4a0daa9d
Semyon Yakimov <sdyakimov@gmail.com>
2021-03-21 19:44:02 +0300

    up

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $(".fancybox").fancybox({
     openEffect: "none",
-    closeEffect: "none"
+    closeEffect: "none",
     padding: 0,
     helpers: {
       overlay: {
